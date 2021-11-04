7bcd950b5c8056359ef8826022ab56044c132a93
nikechervon <kolyachervon@yandex.ru>
2021-11-04 17:22:38 +0500

    Реализовал функционал проверки прав доступа

---
+++ app/Observers/PostObserver.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Observers;
+
+use App\Enums\MediaCollections;
+use App\Models\Post;
+
+class PostObserver
+{
+    /**
+     * @param Post $post
+     */
+    public function deleted(Post $post)
+    {
+//        $post->getFirstMedia(MediaCollections::POSTS)->delete();
+    }
+}
